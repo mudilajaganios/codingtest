93a2cf71a36f9cbbd1832d52c9264dd7ee080613
Jagan Kumar <mudila.jagan@shrenitexpert.online>
2021-01-06 15:30:21 +0400

    Updated Navigation bar tint color

--- Currency Converter/Currency Converter/AppDelegate.swift
+++ Currency Converter/Currency Converter/AppDelegate.swift
@@ -20,6 +20,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         window?.rootViewController = navigationViewController
         window?.makeKeyAndVisible()
         
+        UINavigationBar.appearance().barTintColor = UIColor(red: 234.0/255.0, green: 46.0/255.0, blue: 73.0/255.0, alpha: 1.0)
+        UINavigationBar.appearance().tintColor = UIColor.white
+        UINavigationBar.appearance().titleTextAttributes = [NSAttributedString.Key.foregroundColor : UIColor.white]
+        
         return true
     }
 }
